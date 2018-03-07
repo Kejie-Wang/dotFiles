cabd4de43a17668d690b11c1438df42816b4f681
Kejie-Wang <wang_kejie@foxmail.com>
2018-03-07 14:00:26 +0800

    update

--- install.sh
+++ install.sh
@@ -9,13 +9,19 @@ fi
 
 # add vimrc
 ln -s ${PROJ_DIR}/vim/vimrc ${INSTALL_PREFIX}/.vimrc
-
+mkdir -p ${PROJ_DIR}/vim/dotvim
+ln -s $PROJ_DIR/vim/dotvim $HOME/.vim
 # install vundle to manage vim plugin
 if [ ! -d "${INSTALL_PREFIX}/.vim/bundle/Vundle.vim" ]; then
   echo "Not exist"
   git clone https://github.com/VundleVim/Vundle.vim.git $INSTALL_PREFIX/.vim/bundle/Vundle.vim
 fi
-vim +PluginInstall +qall
+#vim +PluginInstall +qall
+
+# install oh-my-zsh
+mkdir -p $PROJ_DIR/oh-my-zsh
+ln -s $PROJ_DIR/oh-my-zsh $HOME/.oh-my-zsh
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # add zsh conf
 ln -s ${PROJ_DIR}/zsh/zshrc ${INSTALL_PREFIX}/.zshrc
