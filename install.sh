51f2d9ae7c116b459f4f7c8689fd076dd935c82f
Kejie-Wang <wang_kejie@foxmail.com>
2018-03-07 15:11:30 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -10,18 +10,25 @@ fi
 # add vimrc
 ln -s ${PROJ_DIR}/vim/vimrc ${INSTALL_PREFIX}/.vimrc
 mkdir -p ${PROJ_DIR}/vim/dotvim
+if [ -d "$HOME/.vim" ]; then
+  mv $HOME/.vim/* ${PROJ_DIR}/vim/dotvim
+fi
 ln -s $PROJ_DIR/vim/dotvim $HOME/.vim
 # install vundle to manage vim plugin
 if [ ! -d "${INSTALL_PREFIX}/.vim/bundle/Vundle.vim" ]; then
   echo "Not exist"
   git clone https://github.com/VundleVim/Vundle.vim.git $INSTALL_PREFIX/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+  cd ~/.vim/bundle/YouCompleteMe
+  ./install.py --clang-completer
 fi
-#vim +PluginInstall +qall
 
 # install oh-my-zsh
-mkdir -p $PROJ_DIR/oh-my-zsh
+export ZSH=$PROJ_DIR/oh-my-zsh
+if [ ! -d $ZSH ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 ln -s $PROJ_DIR/oh-my-zsh $HOME/.oh-my-zsh
-#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # add zsh conf
 ln -s ${PROJ_DIR}/zsh/zshrc ${INSTALL_PREFIX}/.zshrc
