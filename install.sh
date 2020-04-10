5bba39ffebe196d3c900628fd0218041a36593f6
Kejie-Wang <wang_kejie@foxmail.com>
2020-04-10 11:43:35 +0800

    update vimrc

--- install.sh
+++ install.sh
@@ -9,30 +9,15 @@ fi
 
 # add vimrc
 ln -s ${PROJ_DIR}/vim/vimrc ${INSTALL_PREFIX}/.vimrc
-mkdir -p ${PROJ_DIR}/vim/dotvim
-if [ -d "$HOME/.vim" ]; then
-  mv $HOME/.vim/* ${PROJ_DIR}/vim/dotvim
-fi
-ln -s $PROJ_DIR/vim/dotvim $HOME/.vim
-# install vundle to manage vim plugin
-if [ ! -d "${INSTALL_PREFIX}/.vim/bundle/Vundle.vim" ]; then
-  echo "Not exist"
-  git clone https://github.com/VundleVim/Vundle.vim.git $INSTALL_PREFIX/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-  cd ~/.vim/bundle/YouCompleteMe
-  ./install.py --clang-completer
-fi
+# nvim init.vim
+mkdir -p $INSTALL_PREFIX/.config/nvim
+ln -s ${PROJ_DIR}/vim/vimrc $INSTALL_PREFIX/.config/nvim/init.vim
+# install vim.plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install oh-my-zsh
-export ZSH=$PROJ_DIR/oh-my-zsh
-if [ ! -d $ZSH ]; then
-  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-fi
-ln -s $PROJ_DIR/oh-my-zsh $HOME/.oh-my-zsh
-
-# add zsh conf
-ln -s ${PROJ_DIR}/zsh/zshrc ${INSTALL_PREFIX}/.zshrc
-touch ~/.platform-zshrc
 
 # add gitconfig
 ln -s ${PROJ_DIR}/git/gitconfig ${INSTALL_PREFIX}/.gitconfig
@@ -41,5 +26,15 @@ ln -s ${PROJ_DIR}/git/gitconfig ${INSTALL_PREFIX}/.gitconfig
 ln -s ${PROJ_DIR}/tmux/tmux.conf ${INSTALL_PREFIX}/.tmux.conf
 
 # add pip conf
-mkdir -p ${INSTALL_PREFIX}/.pip
-ln -s ${PROJ_DIR}/pip/pip.conf ${INSTALL_PREFIX}/.pip/pip.conf
+ln -s ${PROJ_DIR}/pip ${INSTALL_PREFIX}/.pip
+
+# add zsh conf
+ln -s ${PROJ_DIR}/zsh/zshrc ${INSTALL_PREFIX}/.zshrc
+touch ~/.platform-zshrc
+
+# install oh-my-zsh
+export ZSH=$PROJ_DIR/oh-my-zsh
+if [ ! -d $ZSH ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+ln -s $PROJ_DIR/oh-my-zsh $HOME/.oh-my-zsh
